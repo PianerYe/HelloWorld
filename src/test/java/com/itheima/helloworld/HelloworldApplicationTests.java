93fbe797982b4f1fac3b3bf47ed9cc847af9289e
yepianer <aimeinvshen@126.com>
2023-02-13 10:26:39 +0800

    测试代码

---
+++ src/test/java/com/itheima/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,14 @@
+package com.itheima.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Hello World！");
+    }
+
+}
