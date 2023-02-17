e8bebbebcb2d3de4607d819170163e2c3ed535d1
yepianer <aimeinvshen@126.com>
2023-02-17 17:05:57 +0800

    在java中操作Redis SpringDataRedis

--- src/test/java/com/itheima/helloworld/com/itheima/test/SpringDataRedisTest.java
+++ src/test/java/com/itheima/helloworld/com/itheima/test/SpringDataRedisTest.java
@@ -4,10 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.SetOperations;
-import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.*;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -16,10 +18,76 @@ public class SpringDataRedisTest {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    /**
+     * 操作String类型数据
+     */
     @Test
-    public void testString(){
-        redisTemplate.opsForValue().set("city123","beijing123");
+    public void testString() {
+        redisTemplate.opsForValue().set("city123", "beijing123");
         String city = (String) redisTemplate.opsForValue().get("city123");
         System.out.println(city);
+
+        redisTemplate.opsForValue().set("key1", "value1", 10l, TimeUnit.SECONDS);
+
+        Boolean aBoolean = redisTemplate.opsForValue().setIfAbsent("city123", "nanjing");
+        System.out.println(aBoolean);
+
     }
+
+    /**
+     * 操作Hash类型数据
+     */
+    @Test
+    public void testHash() {
+        HashOperations hashOperations = redisTemplate.opsForHash();
+
+        //存值
+        hashOperations.put("002", "name", "xiaoming");
+        hashOperations.put("002", "age", "20");
+        hashOperations.put("002", "addr", "beijing");
+
+        //取值
+        String age = (String) hashOperations.get("002", "age");
+        System.out.println(age);
+
+        //获取hash结构中的所有字段
+        Set keys = hashOperations.keys("002");
+        for (Object key : keys) {
+            System.out.println(key);
+        }
+        //获取hash结构中的所有值
+        List values = hashOperations.values("002");
+        for (Object value : values) {
+            System.out.println(value);
+        }
+    }
+
+    /**
+     * 操作list类型数据
+     */
+    @Test
+    public void testList() {
+        ListOperations listOperations = redisTemplate.opsForList();
+
+        //存值
+        listOperations.leftPush("mylist","a");
+        listOperations.leftPushAll("mylist","b","c","d");
+
+        //取值
+        List<String> mylist = listOperations.range("mylist", 0, -1);
+        for (String value : mylist) {
+            System.out.println(value);
+        }
+        //获得列表长度llen
+        Long size = listOperations.size("mylist");
+        int i1 = size.intValue();
+        for (int i = 0; i < i1; i++) {
+            //出队列
+            String mylist1 = (String) listOperations.rightPop("mylist");
+            System.out.println(mylist1);
+        }
+
+
+    }
+
 }
